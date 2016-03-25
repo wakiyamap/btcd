5dbd4f6d5fb298e4d32b11b73ee8bb2a4c5aed4e
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-03-24 21:54:41 -0700

    wire: add segnet's magic bytes

--- wire/protocol.go
+++ wire/protocol.go
@@ -121,6 +121,9 @@ const (
 	// TestNet3 represents the test network (version 3).
 	TestNet3 BitcoinNet = 0x0709110b
 
+	// SegNet represents the segregated witness test network.
+	SegNet BitcoinNet = 0xcaea962e
+
 	// SimNet represents the simulation test network.
 	SimNet BitcoinNet = 0x12141c16
 )
@@ -132,6 +135,7 @@ var bnStrings = map[BitcoinNet]string{
 	TestNet:  "TestNet",
 	TestNet3: "TestNet3",
 	SimNet:   "SimNet",
+	SegNet:   "SegNet",
 }
 
 // String returns the BitcoinNet in human-readable form.
