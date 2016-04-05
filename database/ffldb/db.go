c2288ed913c9324a95816b4d7a7cc5f35ba5a9b9
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-04-05 13:42:48 -0700

    database2: always use witness encoding for block files

--- database/ffldb/db.go
+++ database/ffldb/db.go
@@ -1165,7 +1165,7 @@ func (tx *transaction) StoreBlock(block *btcutil.Block) error {
 		return makeDbErr(database.ErrBlockExists, str, nil)
 	}
 
-	blockBytes, err := block.Bytes()
+	blockBytes, err := block.WitnessBytes()
 	if err != nil {
 		str := fmt.Sprintf("failed to get serialized bytes for block %s",
 			blockHash)
