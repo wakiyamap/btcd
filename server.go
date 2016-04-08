f8ba5259aaa766dc76b999b5312b15a384978f0e
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-04-07 17:29:42 -0700

    set advertised peer ProtocolVersion to wire.WitnessVersion

--- server.go
+++ server.go
@@ -1571,7 +1571,7 @@ func newPeerConfig(sp *serverPeer) *peer.Config {
 		ChainParams:      sp.server.chainParams,
 		Services:         sp.server.services,
 		DisableRelayTx:   cfg.BlocksOnly,
-		ProtocolVersion:  wire.SendHeadersVersion,
+		ProtocolVersion:  wire.WitnessVersion,
 	}
 }
 
