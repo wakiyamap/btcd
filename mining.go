9a7efaac53fef6f39ba5a90d6e4b24b862251298
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-05-15 13:37:10 +0300

    btcd: use median time past for txn finality checks in block assembly

--- mining.go
+++ mining.go
@@ -450,6 +450,13 @@ func NewBlockTemplate(policy *mining.Policy, server *server, payToAddress btcuti
 	minrLog.Debugf("Considering %d transactions for inclusion to new block",
 		len(sourceTxns))
 
+	// Obtain the current median time of the past 11 blocks in order to
+	// evalulate transactions for finality.
+	blockTime, err := blockManager.chain.CalcPastMedianTime()
+	if err != nil {
+		return nil, err
+	}
+
 mempoolLoop:
 	for _, txDesc := range sourceTxns {
 		// A block can't have more than one coinbase or contain
@@ -459,8 +466,9 @@ mempoolLoop:
 			minrLog.Tracef("Skipping coinbase tx %s", tx.Hash())
 			continue
 		}
+
 		if !blockchain.IsFinalizedTransaction(tx, nextBlockHeight,
-			timeSource.AdjustedTime()) {
+			blockTime) {
 
 			minrLog.Tracef("Skipping non-finalized tx %s", tx.Hash())
 			continue
