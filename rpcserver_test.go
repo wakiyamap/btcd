9d732bc6b9dbf3f21853b00395d7c0e53cba0f2f
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-08-22 16:05:49 -0700

    rpctest: add integration test for BIP 113
    This commit adds an integration test using the new rpctest package to
    ensure the implementation of BIP 113 functions as expected.
    
    The test excursus that transactions which are “final” according to the
    latest block-time but not the current MTP are rejected fro the mempool.
    Additionally we check that once a transaction is final according to the
    current MTP, it’s accepted into the mempool and also mined into the
    following block.

--- rpcserver_test.go
+++ rpcserver_test.go
@@ -9,10 +9,16 @@ import (
 	"fmt"
 	"os"
 	"runtime/debug"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/rpctest"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
 )
 
 func testGetBestBlock(r *rpctest.Harness, t *testing.T) {
@@ -91,10 +97,153 @@ func testGetBlockHash(r *rpctest.Harness, t *testing.T) {
 	}
 }
 
+func testMedianTimePastLockTime(r *rpctest.Harness, t *testing.T) {
+	// We'd like to test the proper adherance of the BIP 113 rule
+	// constraint which requires all transaction finality tests to use the
+	// MTP of the last 11 blocks, rather than the timestamp of the block
+	// which includes them.
+
+	// First, create a series of timestamps we'll use. Each block has a
+	// timestamp exactly 10 minutes after the previous block.
+	const medianTimeBlocks = 11
+	timeStamps := make([]time.Time, medianTimeBlocks)
+	timeStamps[0] = time.Now().Add(time.Minute)
+	for i := 1; i < medianTimeBlocks; i++ {
+		timeStamps[i] = timeStamps[i-1].Add(time.Minute * 10)
+	}
+
+	// With the stamps created, generate a series of empty blocks for each
+	// of the timestamps.
+	blocks := make([]*btcutil.Block, 0, len(timeStamps))
+	for _, timeStamp := range timeStamps {
+		block, err := r.GenerateAndSubmitBlock(nil, -1, timeStamp)
+		if err != nil {
+			t.Fatalf("unable to generate block: %v", err)
+		}
+		blocks = append(blocks, block)
+	}
+
+	// The current MTP should be the timestamp of 6th block generated.
+	currentMTP := blocks[5].MsgBlock().Header.Timestamp
+
+	// Fetch a fresh address from the harness, we'll use this within the
+	// transaction we create below shortly.
+	addr, err := r.NewAddress()
+	if err != nil {
+		t.Fatalf("unable to generate address: %v", err)
+	}
+	addrScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		t.Fatalf("unable to generate addr script: %v", err)
+	}
+
+	// Create a fresh key, then send some coins to an address spendable by
+	// that key.
+	key, err := btcec.NewPrivateKey(btcec.S256())
+	if err != nil {
+		t.Fatalf("unable to generate key: %v", err)
+	}
+	a, err := btcutil.NewAddressPubKey(key.PubKey().SerializeCompressed(), r.ActiveNet)
+	if err != nil {
+		t.Fatalf("unable to create address: %v", err)
+	}
+	selfAddrScript, err := txscript.PayToAddrScript(a.AddressPubKeyHash())
+	if err != nil {
+		t.Fatalf("unable to generate addr script: %v", err)
+	}
+	output := &wire.TxOut{PkScript: selfAddrScript, Value: 1e8}
+	fundTx, err := r.CreateTransaction([]*wire.TxOut{output}, 10)
+	if err != nil {
+		t.Fatalf("unable to send outputs: %v", err)
+	}
+	if _, err := r.Node.SendRawTransaction(fundTx, true); err != nil {
+		t.Fatalf("Unable to broadcast transaction: %v", err)
+	}
+
+	// Locate the output index of the coins spendable by the key we
+	// generated above. We'll need this to craft a new transaction shortly
+	// below.
+	var outputIndex uint32
+	if bytes.Equal(fundTx.TxOut[0].PkScript, selfAddrScript) {
+		outputIndex = 0
+	} else {
+		outputIndex = 1
+	}
+
+	// Now create a transaction with a lock time which is "final" according
+	// to the latest block, but not according to the current median time
+	// past.
+	tx := wire.NewMsgTx()
+	tx.LockTime = uint32(blocks[len(blocks)-1].MsgBlock().Header.Timestamp.Unix())
+	tx.AddTxIn(&wire.TxIn{
+		PreviousOutPoint: wire.OutPoint{
+			Hash:  fundTx.TxHash(),
+			Index: outputIndex,
+		},
+	})
+	tx.AddTxOut(&wire.TxOut{
+		PkScript: addrScript,
+		Value:    btcutil.SatoshiPerBitcoin - 1000,
+	})
+	sigScript, err := txscript.SignatureScript(tx, 0, selfAddrScript,
+		txscript.SigHashAll, key, true)
+	if err != nil {
+		t.Fatalf("unable to generate sig: %v", err)
+	}
+	tx.TxIn[0].SignatureScript = sigScript
+
+	// This transaction should be rejected. Additionally, the exact error
+	// should be the rejection of a non-final transaction.
+	_, err = r.Node.SendRawTransaction(tx, true)
+	if err == nil {
+		t.Fatalf("transaction accepted, but should be non-final")
+	} else if !strings.Contains(err.Error(), "not finalized") {
+		t.Fatalf("transacvtion should be rejected due to being "+
+			"non-final, instead :%v", err)
+	}
+
+	// Modify the transaction to have a valid lock-time considered "final"
+	// according to the current MTP.
+	tx.LockTime = uint32(currentMTP.Unix()) - 30
+	sigScript, err = txscript.SignatureScript(tx, 0, selfAddrScript,
+		txscript.SigHashAll, key, true)
+	if err != nil {
+		t.Fatalf("unable to generate sig: %v", err)
+	}
+	tx.TxIn[0].SignatureScript = sigScript
+
+	// This transaction should now be accepted to the mempool.
+	txid, err := r.Node.SendRawTransaction(tx, true)
+	if err != nil {
+		t.Fatalf("unable to send transaction: %v", err)
+	}
+
+	// Mine a single block, the transaction within the mempool should now
+	// be within this block.
+	blockHash, err := r.Node.Generate(1)
+	if err != nil {
+		t.Fatalf("unable to generate block: %v", err)
+	}
+	block, err := r.Node.GetBlock(blockHash[0])
+	if err != nil {
+		t.Fatalf("unable to obtain block: %v", err)
+	}
+	for _, txn := range block.Transactions() {
+		if txn.Hash().IsEqual(txid) {
+			return
+		}
+	}
+
+	// If we've reeached this point, then our time-locked transaction
+	// wasn't selected to be included within this block.
+	t.Fatalf("lock-time transaction not included within block")
+}
+
 var rpcTestCases = []rpctest.HarnessTestCase{
 	testGetBestBlock,
 	testGetBlockCount,
 	testGetBlockHash,
+	testMedianTimePastLockTime,
 }
 
 var primaryHarness *rpctest.Harness
