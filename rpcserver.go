e8effd760354dcd2d218afa98edd447156a1d6c9
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-12-06 18:12:44 -0800

    rpcserver: add csv case for getblockchaininfo

--- rpcserver.go
+++ rpcserver.go
@@ -1201,6 +1201,8 @@ func handleGetBlockChainInfo(s *rpcServer, cmd interface{}, closeChan <-chan str
 		switch deployment {
 		case chaincfg.DeploymentTestDummy:
 			forkName = "dummy"
+		case chaincfg.DeploymentCSV:
+			forkName = "csv"
 		default:
 			return nil, &btcjson.RPCError{
 				Code: btcjson.ErrRPCInternal.Code,
